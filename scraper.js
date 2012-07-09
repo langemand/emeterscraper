bd3d241353fbfa044f2f5d673d7304fa82f7a9d3
langemand <henrik.scheel@spjeldager.dk>
2012-07-09 20:44:43 +0200

    Removes dublet images at init

--- scraper.js
+++ scraper.js
@@ -16,18 +16,39 @@ var lastImage = null;
 var lastAcquire = null;
 
 function init() {
+    var dublets=new Array();
+    var icnt=0;
+    var imageBefore=null;
+
     db.find({}, function(err, cursor) {
         if (err) {
             console.log("scraper.init: db.find got error: " + err);
         } else {
             cursor.each(function (err, item) {
                 if (item ) {
+                    // register dublets
+                    if ( imageBefore && item.image.toString() == imageBefore.toString() ) {
+                        dublets[icnt] = item.timestamp;
+                        icnt++;
+                    } else {
+//console.log("image: " + item.image);
+//console.log("before:" + imageBefore);
+                        imageBefore = item.image;
+                    }
+
+                    // register newest
                     if ( item.timestamp > lastAcquire ) {
                     	lastImage = item.image;
                     	lastAcquire = item.timestamp;
                     }
                 } else {
-                    console.log("Scraper.init: Newest timestamp is from " + lastAcquire);
+                    console.log("Scraper.init: Newest timestamp is from " + lastAcquire + ", " + icnt + " dublets found");
+                    for (i=0; i<icnt; i++)
+                    {
+                         db.remove({timestamp: dublets[i]}, function(err, result) {
+                             if (!err) console.log('Dublet deleted');
+                         });
+                    }
                 }
             });
         }
